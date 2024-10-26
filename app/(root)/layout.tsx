5fcb2bcceed86101c51140ef2ff24283801a774c
Thiago Sawada <tms1_tms2@hotmail.com>
2024-10-25 23:05:38 -0300

    feat: create Navbar and Footer files

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      Navbar
+      <Navbar />
       {children}
-      Footer
+      <Footer />
     </>
   )
 }
